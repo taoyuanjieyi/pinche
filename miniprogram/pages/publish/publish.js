eb25f2432f27df0571fbd35b155b38ce02839f7b
FAREAST\v-jielia <v-jielia@microsoft.com>
2018-11-14 14:51:58 +0800

    change publish page

--- miniprogram/pages/publish/publish.js
+++ miniprogram/pages/publish/publish.js
@@ -1,73 +1,62 @@
 // pages/publish/publish.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    array: ['1', '2', '3', '4', '5', '6'],
+    objectArray: [{
+        id: 0,
+        name: '1'
+      },
+      {
+        id: 1,
+        name: '2'
+      },
+      {
+        id: 2,
+        name: '3'
+      },
+      {
+        id: 3,
+        name: '4'
+      },
+      {
+        id: 4,
+        name: '5'
+      },
+      {
+        id: 5,
+        name: '6'
+      }
+    ]
+  },
+  bindPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  bindDateChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  bindTimeChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      time: e.detail.value
+    })
+  },
+  bindRegionChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      region: e.detail.value
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  formBindsubmit: function(e) {
+    if (e.detail.value.nickname === null || e.detail.value.nickname === '') {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-
-  formBindsubmit: function (e) {
-    if (e.detail.value.nickname === null || e.detail.value.nickname === ''){
-
-    } else if (e.detail.value.start === null || e.detail.value.start === ''){
+    } else if (e.detail.value.start === null || e.detail.value.start === '') {
 
     } else if (e.detail.value.end === null || e.detail.value.end === '') {
 
@@ -79,7 +68,7 @@ Page({
 
     } else if (e.detail.value.carinfo === null || e.detail.value.carinfo === '') {
 
-    }else{
+    } else {
       // console.log(e.detail.value.nickname);
       // console.log(e.detail.value.start);
       // console.log(e.detail.value.end);
