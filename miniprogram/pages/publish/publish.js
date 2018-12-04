94bf78d22327a026eac6c49bf662d69607a821fc
jianxing.djx <jianxing.djx@antfin.com>
2018-12-05 01:33:45 +0800

    增加行程发布时间限制

--- miniprogram/pages/publish/publish.js
+++ miniprogram/pages/publish/publish.js
@@ -11,7 +11,8 @@ const nowDay = date.getDate()
 const nowHour = date.getHours()
 const nowMinute = date.getMinutes()
 const nowSecond = date.getSeconds()
-
+const nowDate = nowYear +"-"+ (nowMonth<10?"0":"") +nowMonth +"-"+ (nowDay<10?"0":"")+nowDay;
+const nowTime = (nowHour < 10 ? "0" : "") + nowHour + ":" + (nowMinute < 10 ? "0" : "") + nowMinute;
 Page({
   data: {
     vacancy:0,
@@ -24,6 +25,7 @@ Page({
     hour: nowHour,
     minute: nowMinute,
     second: nowSecond,
+    minDate:nowDay,
     value: [9999, 1, 1],
     array: ['1', '2', '3', '4', '5', '6'],
     objectArray: [{
@@ -53,15 +55,26 @@ Page({
     ]
   },
   onLoad:function(e){
-    //setDate(this.data.year, this.data.month, this.data.day, this)
+    console.info("当前系统日期：", nowDate, nowTime);
+    this.setData({
+      minDate: nowDate,
+      minTime: nowTime
+    })
   },
   bindDateChange: function(e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
-    let val = e.detail.value
-  //  setDate(this.data.years[val[0]], this.data.months[val[1]], this.data.days[val[2]], this)
-    this.setData({
-      date: e.detail.value
-    })
+    if (e.detail.value === nowDate){
+      this.setData({
+        minTime: nowTime,
+        date: e.detail.value
+      })
+    }else{
+      this.setData({
+        minTime: "00:01",
+        date: e.detail.value
+      })
+    }
+    
   },
   bindTimeChange: function(e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
@@ -78,7 +91,6 @@ Page({
 
   formBindsubmit: function (e) {
     console.info("表单数据：", e.detail.value);
-    console.info("数据：", this.data)
     if (this.data.date === null || this.data.date === '' || this.data.date === undefined) {
       wx.showToast({
         icon: 'none',
@@ -110,14 +122,6 @@ Page({
       return;
     }
 
-      // console.log(e.detail.value.nickname);
-      // console.log(e.detail.value.start);
-      // console.log(e.detail.value.end);
-      // console.log(e.detail.value.body);
-      // console.log(e.detail.value.vacancy);
-      // console.log(e.detail.value.departureTime);
-      // console.log(e.detail.value.carinfo);
-      // console.log(e.detail.value.remark);
       var timestamp = Date.parse(new Date());
     driverRequest.publish({
       passPoint: e.detail.value.body,
