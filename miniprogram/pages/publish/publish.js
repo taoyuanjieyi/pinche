f78326dd40bf30d315c294e30643b15c7132859f
FAREAST\v-jielia <v-jielia@microsoft.com>
2018-12-13 13:14:47 +0800

    add car message

--- miniprogram/pages/publish/publish.js
+++ miniprogram/pages/publish/publish.js
@@ -11,11 +11,11 @@ const nowDay = date.getDate()
 const nowHour = date.getHours()
 const nowMinute = date.getMinutes()
 const nowSecond = date.getSeconds()
-const nowDate = nowYear +"-"+ (nowMonth<10?"0":"") +nowMonth +"-"+ (nowDay<10?"0":"")+nowDay;
+const nowDate = nowYear + "-" + (nowMonth < 10 ? "0" : "") + nowMonth + "-" + (nowDay < 10 ? "0" : "") + nowDay;
 const nowTime = (nowHour < 10 ? "0" : "") + nowHour + ":" + (nowMinute < 10 ? "0" : "") + nowMinute;
 Page({
   data: {
-    vacancy:0,
+    vacancy: 0,
     years: [],
     months: [],
     days: [],
@@ -25,35 +25,35 @@ Page({
     hour: nowHour,
     minute: nowMinute,
     second: nowSecond,
-    minDate:nowDay,
+    minDate: nowDay,
     value: [9999, 1, 1],
     array: ['1', '2', '3', '4', '5', '6'],
-    body:"",
+    body: "",
     /*车辆颜色*/
-    carColor:['红色','黑色','白色'],
+    carColor: ['黑色', '白色', '红色', '金色', '灰色', '银色', '黄色', '蓝色', '棕色'],
     /*车辆品牌*/
-    carBrand: ['大众', '奔驰', '宝马'],
+    carBrand: ['奥迪', '奔驰', '本田', '别克', '比亚迪', '标志', '长安', '长城', '银色', '传祺', '大众', '丰田', '福特', '菲亚特', '哈佛', '红旗', '海马', '吉利', 'JEEP', '凯迪拉克', '克莱斯勒', '雷克萨斯', '路虎', '林肯', '雷诺', '陆风', '马自达', '名爵', '尼桑', '欧宝', '奇瑞', '起亚', '荣威', '绅宝', '三菱', '斯柯达', '斯巴鲁', '沃尔沃', '现代', '雪佛兰', '雪铁龙', '英菲尼迪', '众泰', '中华', '其他'],
     /*textarea */
-    min: 5,//最少字数
+    min: 5, //最少字数
     max: 200, //最多字数 
   },
-  onLoad:function(e){
+  onLoad: function(e) {
     console.info("当前系统日期：", nowDate, nowTime);
     this.setData({
       minDate: nowDate,
       minTime: nowTime,
-      time:nowTime,
-      date:nowDate,
+      time: nowTime,
+      date: nowDate,
       submitBtnClass: "publish-btn"
     })
   },
   bindDateChange: function(e) {
-    if (e.detail.value === nowDate){
+    if (e.detail.value === nowDate) {
       this.setData({
         minTime: nowTime,
         date: e.detail.value
       })
-    }else{
+    } else {
       this.setData({
         minTime: "00:01",
         date: e.detail.value
@@ -74,20 +74,20 @@ Page({
     this.changeSubmitStyle()
   },
   /*车牌颜色*/
-  bindColorChange: function (e) {
+  bindColorChange: function(e) {
     this.setData({
       color: e.detail.value
     })
     this.changeSubmitStyle()
   },
   /*车辆品牌*/
-  bindBrandChange: function (e) {
+  bindBrandChange: function(e) {
     this.setData({
       brand: e.detail.value
     })
     this.changeSubmitStyle()
   },
-  bindBodyChange: function (e) {
+  bindBodyChange: function(e) {
     // 获取输入框的内容
     var value = e.detail.value;
     // 获取输入框内容的长度
@@ -104,7 +104,7 @@ Page({
     })
     this.changeSubmitStyle()
   },
-  changeSubmitStyle:function(){
+  changeSubmitStyle: function() {
     let changeSubmitBtnEnable = true;
     if (this.data.date === null || this.data.date === '' || this.data.date === undefined) {
       changeSubmitBtnEnable = false;
@@ -120,18 +120,18 @@ Page({
       this.data.vacancy === undefined) {
       changeSubmitBtnEnable = false;
     }
-    if (changeSubmitBtnEnable){
+    if (changeSubmitBtnEnable) {
       this.setData({
-        submitBtnClass:"publish-btn publish-btn-active"
+        submitBtnClass: "publish-btn publish-btn-active"
       })
-    }else{
+    } else {
       this.setData({
         submitBtnClass: "publish-btn"
       })
     }
   },
 
-  formBindsubmit: function (e) {
+  formBindsubmit: function(e) {
     console.info("表单数据：", e.detail.value);
     if (this.data.date === null || this.data.date === '' || this.data.date === undefined) {
       wx.showToast({
@@ -155,7 +155,7 @@ Page({
       })
       return;
     }
-    if (this.data.vacancy === null || this.data.vacancy === '' || 
+    if (this.data.vacancy === null || this.data.vacancy === '' ||
       this.data.vacancy === undefined) {
       wx.showToast({
         icon: 'none',
@@ -164,13 +164,13 @@ Page({
       return;
     }
 
-      var timestamp = Date.parse(new Date());
+    var timestamp = Date.parse(new Date());
     driverRequest.publish({
       passPoint: e.detail.value.body,
-      startTime: this.data.date + " " + this.data.time+":00",
+      startTime: this.data.date + " " + this.data.time + ":00",
       vacancy: this.data.array[this.data.vacancy],
     }).then((res) => {
-      if (res.retCode === 'success' ) {
+      if (res.retCode === 'success') {
         wx.showToast({
           icon: 'none',
           title: '发布成功',
@@ -178,7 +178,7 @@ Page({
         wx.switchTab({
           url: '/pages/index/index'
         })
-      }else{
+      } else {
         wx.showToast({
           icon: 'none',
           title: res.retMsg
