65beed64949f1e01999922bc15711d148aae6404
FAREAST\v-jielia <v-jielia@microsoft.com>
2018-12-13 16:30:40 +0800

    add common stroke

---
+++ miniprogram/pages/commonStroke/commonStroke.js
@@ -0,0 +1,12 @@
+Page({
+  data: {
+    items: [
+      { name: '行程一', value: '中关村—顺义石门中关村—顺义石门中关村—顺义石门中关村—顺义石门中关村—顺义石门中关村—顺义石门中关村—顺义石门中关村—顺义石门中关村—顺义石门中关村—顺义石门中关村—顺义石门中关村—顺义石门中关村—顺义石门' },
+      { name: '行程二', value: '河北香河—北京', checked: 'true' },
+      { name: '行程三', value: '通州—石家庄' }
+    ]
+  },
+  radioChange(e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value)
+  }
+})
