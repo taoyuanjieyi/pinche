3c8b6f4073e70ee50a15f51f22ef4008d3ae885b
jianxing.djx <jianxing.djx@antfin.com>
2018-12-22 20:14:10 +0800

    功能细节优化

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -16,7 +16,8 @@ Page({
     ownerRouteList : [] ,
     joinRouteList : [],
     pageNumber: 1,
-    pageSize: 5,
+    pageSize: 10,
+    totalPages:0,
     keyword: "",
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
@@ -27,21 +28,19 @@ Page({
     })
   },
   onLoad: function () {
-    
+    var that = this;
+    // 检查是否登录,登录成功执行queryRouteList()方法
+    login.checkLogin(function () {
+      that.queryRouteList()
+    }, true);
   },
   onShow: function () {
-    var that  = this;
-    that.setData({
-      pageNumber: 1,
-      pageSize: 5,
-    })
-    // 检查是否登录,登录成功执行queryRouteList()方法
-    login.checkLogin(function(){
-      that.queryRouteList()
-    },true);
+    
   },
   // 下拉刷新
   onPullDownRefresh: function () {
+    //清空页面已加载的数据
+    this.clearPageData()
     // 显示顶部刷新图标
     wx.showNavigationBarLoading();
     var that = this;
@@ -76,12 +75,20 @@ Page({
       url: '../seat/seat?isPassenger=true&routeId=' + this.data.joinRouteList[index].routeId
     })
   },
+  clearPageData:function(){
+    this.setData({
+      driverRouteList: [],
+      ownerRouteList: [],
+      joinRouteList: [],
+      pageNumber: 1,
+      pageSize: 10,
+    })
+  },
   bindSearchChange:function(e){
     var value = e.detail.value;
+    this.clearPageData()
     this.setData({
-      keyword:value,
-      pageNumber:1,
-      pageSize:5,
+      keyword: value,
     })
     var len = parseInt(value.length);
     this.queryDriverRouteList({
@@ -102,8 +109,10 @@ Page({
       var driverList = this.data.driverRouteList
       var ownerList = this.data.ownerRouteList
       var joinList = this.data.joinRouteList
+      var totalPages = 0;
       if (res.data.retCode === "success") {
         if (res.data.page.list != null && res.data.page.list !== undefined && res.data.page.list.length>0){
+          totalPages = res.data.page.pages
           for (var i=0;i<res.data.page.list.length;i++){
             driverList.push(res.data.page.list[i]);
           }
@@ -113,6 +122,7 @@ Page({
         driverRouteList: driverList,
         ownerRouteList: res.data.ownerRouteList,
         joinRouteList: res.data.joinRouteList,
+        totalPages: totalPages,
       })
       // 隐藏加载框  
       wx.hideLoading();
@@ -122,15 +132,21 @@ Page({
   // 上拉加载更多
   onReachBottom: function () {
     var that = this;
-    // 显示加载图标  
-    wx.showLoading({
-      title: '玩命加载中',
-    })
     // 页数+1 
-    this.setData({
-      pageNumber: this.data.pageNumber + 1
-    })
-    this.queryRouteList();
+    var nextPageNumber = this.data.pageNumber + 1;
+    if(nextPageNumber<=this.data.totalPages){
+      // 显示加载图标  
+      wx.showLoading({
+        title: '玩命加载中',
+      })
+      this.setData({
+        pageNumber: nextPageNumber
+      })
+      this.queryRouteList();
+    }else{
+      // 提示已经到最后一页
+      console.info("已经是最后一页了哦...")
+    }
   },
   // 点击返回到顶部
   onTabItemTap(item) {
